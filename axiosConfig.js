e32a32fbb9c1d9a356f86719b6f9dde563dfe8bb
noboomta <puvana.jp36@gmail.com>
2021-10-24 19:02:44 +0700

    add more Suites

--- axiosConfig.js
+++ axiosConfig.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const axiosInstance = axios.default.create({
     baseURL: 'https://suchonsite-server.herokuapp.com/',
     herokuPeopleAll: 'https://suchonsite-server.herokuapp.com/people/all',
-    herokuPeopleByDate: 'https://suchonsite-server.herokuapp.com/people/by_date/'
+    herokuPeopleByDate: 'https://suchonsite-server.herokuapp.com/people/by_date/',
+    herokuGetDataFromGov: 'https://suchonsite-server.herokuapp.com/getDataFromGov/'
 });
 
 module.exports = axiosInstance;
